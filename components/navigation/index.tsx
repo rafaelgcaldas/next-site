eeeca8a4bf7d5b6951e1099b991552952c58fdb4
rafaelgcaldas01 <rafael.gcaldas01@gmail.com>
2021-01-11 23:30:12 -0300

    Navigation component

---
+++ components/navigation/index.tsx
@@ -0,0 +1,47 @@
+import { useRouter } from 'next/router'
+import  Link  from 'next/link'
+import { ReactNode } from 'react'
+
+import styles from './navigation.module.css'
+
+const LINK = [
+    {
+        name: 'Home',
+        path: '/'
+    },
+    {
+        name: "About",
+        path: '/about'
+    }
+]
+
+type NavAnchor = {
+    path: string
+    children: ReactNode
+}
+
+function NavAnchor ({path, children }: NavAnchor) {
+    return (
+        <Link href={path} >
+            <a className={ styles.navAnchor }>{ children }</a>
+        </Link>
+    )
+}
+
+export default function Navigation() {
+    const { pathname } = useRouter();
+
+    return (
+        <nav>
+            <ul className={ styles.list }>
+                {LINK.map(({ name, path }) => (
+                    <li key={path}>
+                        { path === pathname 
+                            ? <span>{ name }</span> 
+                            : <NavAnchor path={ path }>{ name }</NavAnchor> }
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    )
+}
